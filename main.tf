99687dea5bb7ae9e44cd183324d6aa4b2f8965d9
osawa-koki <github@osawa.dev>
2023-02-17 21:37:54 +0900

    小さな修正。

--- main.tf
+++ main.tf
@@ -118,7 +118,7 @@ resource "aws_security_group" "example" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
