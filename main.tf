ffed262cff4c1987d809862b9affdf3dd0a292bb
osawa-koki <github@osawa.dev>
2023-02-17 20:57:26 +0900

    EC2

--- main.tf
+++ main.tf
@@ -20,6 +20,12 @@ variable "password" {
   description = "データベースのパスワード"
 }
 
+variable "ssh_public_key_path" {
+  type = string
+  default = "~/.ssh/id_rsa.pub"
+  description = "Path to the public key."
+}
+
 # プロバイダーを設定
 provider "aws" {
   region = "ap-northeast-1"
@@ -28,6 +34,12 @@ provider "aws" {
 # VPCを定義
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
+}
+
+# サブネットを定義
+resource "aws_subnet" "example" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.0.0/24"
 }
 
 # インターネットゲートウェイを定義
@@ -61,6 +73,28 @@ resource "aws_security_group" "example" {
   vpc_id      = aws_vpc.example.id
 
   ingress {
+    description      = "SSH connection"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["${var.allowed_ip_address}/32"]
+  }
+  ingress {
+    description      = "HTTP connection"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "HTTPS connection"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Amazon DocumentDB connection"
     from_port   = 27017
     to_port     = 27017
     protocol    = "tcp"
@@ -69,14 +103,17 @@ resource "aws_security_group" "example" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    "name" = "${var.project_name}-security-group"
   }
 }
 
 # DocumentDBクラスターを定義
 resource "aws_docdb_cluster" "example" {
-  cluster_identifier      = "${replace(var.project_name, "_", "-")}-docdb"
+  cluster_identifier      = "${replace(var.project_name, "_", "-")}-docdb-cluster"
   engine                  = "docdb"
   master_username         = var.username
   master_password         = var.password
@@ -92,6 +129,30 @@ resource "aws_docdb_cluster_instance" "example" {
   cluster_identifier = aws_docdb_cluster.example.id
   instance_class     = "db.t3.medium"
   count              = 1
+}
+
+# キーペアを定義
+resource "aws_key_pair" "example" {
+  key_name   = "${var.project_name}-key"
+  public_key = file(var.ssh_public_key_path)
+}
+
+# Elastic IPを定義
+resource "aws_eip" "example" {
+  vpc = true
+}
+
+# EC2インスタンスを定義
+resource "aws_instance" "example" {
+  ami = "ami-be4a24d9"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.example.id]
+  subnet_id = aws_subnet.example.id
+  associate_public_ip_address = true
+  tags = {
+    Name = "${var.project_name}-ec2"
+  }
 }
 
 resource "local_file" "connection_string" {
